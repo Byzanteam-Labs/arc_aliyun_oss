13f2a832506f9e8f768aeb4b4a2061c795d4a9de
fahchen <06fahchen@gmail.com>
2019-07-11 16:09:01 +0800

    Bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ArcAliyunOss.MixProject do
   use Mix.Project
 
   @project_host "https://github.com/GreenNerd-Labs/arc_aliyun_oss"
-  @version "0.2.0"
+  @version "0.2.1"
 
   def project do
     [
